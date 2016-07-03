f28aa209c4d51be618d797404f75bd398c33d74f
kaisersource <emanuelebosimini@gmail.com>
2016-07-03 21:28:17 +0200

    Update Floyd_Warshall.c

--- Floyd_Warshall/Floyd_Warshall.c
+++ Floyd_Warshall/Floyd_Warshall.c
@@ -1,12 +1,23 @@
 
-void floyd_warshall() {
-	int i, j, k;
-	for (i = 1; i <= N; i++)
-		for (j = 1; j <= N; j++)
-			D[i][j] = W[i][j]; // W[i][i] = 0
+void Floyd_Farshall(int *D, int *pi,int n) {//D e' la matdelle distanze, pi la matr dei predecessori, W i pesi degli archi, n dim delle mat
+	int i, j, k,*w;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++){
+			D[i+j*n] = w[i+j*n]; // W[i][i] = 0 procedura tramite array : D[i+n*j]=w[i+n*j];
+			if((i==j)||(w[i+n*j]==1000*n))
+				pi[i+n*j]=n;
+			else
+				pi[i+n*j]=i;
+		}
 
-	for (k = 1; k <= N; k++)
-		for (i = 1; i <= N; i++)
-			for (j = 1; j <= N; j++)
-				D[i][j] = min(D[i][j], D[i][k] + D[k][j]);
+	for (k = 0; k < n; k++)
+		for (i = 0; i < n; i++)
+			for (j = 0; j < n; j++)
+			if((D[i+k*n]!=n*1000)&&(D[k+n*j]!=n*1000))
+				if(D[i+n*j]>D[i+k*n]+D[k+n*j]){
+					D[i+n*j]=D[i+k*n]+D[k+n*j];
+					pi[i+n*j]=pi[k+n*j];
+				}
+
 }
+//costo spaziale: O(n^3) dove n= |V| e' la cardinalita' dei vertici
